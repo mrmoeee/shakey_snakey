8ca1c68904ffea5e32eb0c91d6458835d97a214c
matt_moe <mrobertmoe@gmail.com>
2018-08-22 21:41:00 -0700

    start asteroid creation

---
+++ src/asteroids.js
@@ -0,0 +1,19 @@
+
+class Asteroid {
+  constructor() {
+    //all zeroes to be determined later
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 0;
+    this.yspeed = 0;
+    
+  }
+
+  update() {
+
+  }
+
+  show() {
+
+  }
+}
